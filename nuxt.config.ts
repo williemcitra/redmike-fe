05604080ed5dff45602952e033d123349c537e09
William <william@aktivaku.com>
2024-03-21 17:28:26 +0700

    add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-medusa',  'nuxt-primevue'],
+  modules: ['nuxt-medusa',  'nuxt-primevue', '@pinia/nuxt'],
   css: ['/node_modules/primeflex/primeflex.css', '@/assets/css/style.css'],
-  ssr: true
+  ssr: false
 })
